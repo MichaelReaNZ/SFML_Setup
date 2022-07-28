74e311757eb769bb8cc1fd141084e6a965a45820
michaelreanz <MichaelReaNZ@gmail.com>
2022-07-28 04:57:28 +0000

    Circles change color on click

--- SFMLSetup/main.cpp
+++ SFMLSetup/main.cpp
@@ -1,10 +1,11 @@
 #include "Object.h"
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 
-    Object* newCircle = new Object(sf::Vector2f(0.0f, 0.0f), 50, sf::Color::Green);
+    Object* greenCircle = new Object(sf::Vector2f(0.0f, 0.0f), 50, sf::Color::Green);
     Object* newCircle2 = new Object(sf::Vector2f(300.0f, 50.0f), 50, sf::Color::Blue);
 
     while (window.isOpen())
@@ -17,7 +18,24 @@ int main()
         }
 
         window.clear();
-        newCircle->Update(window);
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		  {
+				std::cout << "Left mouse button pressed" << std::endl;
+				if(greenCircle->m_Shape->getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
+				{
+					//change color red
+				   greenCircle->m_Shape->setFillColor(sf::Color::Red);
+				}
+            else {
+					//change color green
+               greenCircle->m_Shape->setFillColor(sf::Color::Green);
+            }
+		  }
+		
+	 
+		  
+		
+        greenCircle->Update(window);
         newCircle2->Update(window);
         //window.draw(shape);
         window.display();
